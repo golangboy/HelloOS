3adfb3c874d93cea13823820e3a3cb64d78e77b3
golangboy <golangboy@qq.com>
2022-08-12 22:45:19 +0800

    欢迎提示

--- entry.c
+++ entry.c
@@ -1,11 +1,7 @@
+#include "console.h"
 int entry()
 {
-    char *vga = 0xb8000;
-    char *ptr = "Hello Kernel!";
-    for (int i = 0; *ptr; i += 2, ptr++)
-    {
-        vga[i] = *ptr;
-    }
+    console_printf("HelloOS!");
     while (1)
         ;
     return 0;
