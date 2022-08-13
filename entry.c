2c09ee6f09922aa79b665a417854fe9cceded04f
golangboy <golangboy@qq.com>
2022-08-13 12:44:32 +0800

    支持查询符号名

--- entry.c
+++ entry.c
@@ -2,12 +2,14 @@
 #include "multiboot.h"
 #include "idt.h"
 #include "timer.h"
+#include "elf.h"
 int entry(struct multiboot_t *m)
 {
     console_printf("HelloOS!");
     load_idt();
     init_8259();
     init_timer(100);
+    init_sym(m);
     asm volatile("sti");
     while (1)
         ;
