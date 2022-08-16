020ebf8ab2f3f668e33bd86886c5d3686e192786
golangboy <golangboy@qq.com>
2022-08-16 13:34:55 +0800

    支持panic

--- debug.c
+++ debug.c
@@ -1,6 +1,7 @@
 #include "debug.h"
 #include "types.h"
 #include "elf.h"
+#include "console.h"
 void print_stack_trace()
 {
     uint32_t *ebp, *eip;
@@ -13,4 +14,14 @@ void print_stack_trace()
         console_printf("   [%x] %s\n", *eip, lookup_sym(*eip));
         ebp = (uint32_t *)*ebp;
     }
+}
+void panic(char *str)
+{
+    console_write_color("\n\nPANIC: ", rc_black, rc_red);
+    console_write_color(str, rc_black, rc_red);
+    console_write_color("\n\n", rc_black, rc_red);
+    print_stack_trace();
+    asm volatile("cli");
+    while (1)
+        ;
 }
--- debug.h
+++ debug.h
@@ -1,4 +1,5 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 void print_stack_trace();
+void panic(char *str);
 #endif
