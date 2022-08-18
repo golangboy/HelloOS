6037fdd79ad144a23dd4b55ebd436906854fe766
golangboy <golangboy@qq.com>
2022-08-18 13:51:47 +0800

    增加字符串操作函数

--- strings.c
+++ strings.c
@@ -20,3 +20,14 @@ int strcmp(char *str1, char *str2)
     }
     return 1;
 }
+int strcpy(char *str1, char *str2)
+{
+    int i = 0;
+    while (str2[i] != '\0')
+    {
+        str1[i] = str2[i];
+        i++;
+    }
+    str1[i] = '\0';
+    return 0;
+}
--- strings.h
+++ strings.h
@@ -1,4 +1,5 @@
 #ifndef STRING_H
 #define STRING_H
 int strcmp(char *str1, char *str2);
+int strcpy(char *str1, char *str2);
 #endif
