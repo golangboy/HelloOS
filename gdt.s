3802d197734c103360b0c6f17d13b92b5265ecd0
golangboy <golangboy@qq.com>
2022-08-18 15:43:11 +0800

    修改gdt

--- gdt.s
+++ gdt.s
@@ -2,20 +2,19 @@
 global reload_gdt
 section .text
 reload_gdt:
-    ;lgdt [gdt_ptr+0xc0000000]
-    sgdt [gdt_ptr]
-    mov ebx , [gdt_ptr+0x2]
-    add dword  [gdt_ptr+0x2] ,0xc0000000
-    lgdt [gdt_ptr]
+    lgdt [gdt_ptr+0xc0000000]
+    ; sgdt [gdt_ptr]
+    ; mov ebx , [gdt_ptr+0x2]
+    ; add dword  [gdt_ptr+0x2] ,0xc0000000
+    ; lgdt [gdt_ptr]
     ret
 
 section .data
 gdt_ptr:
-    dw 5*8-1
+    dw 4*8-1
     dd gdt_table
 gdt_table:
     dq 0
     dq 0
     dq 0xCF9A000000FFFF
     dq 0xCF92000000FFFF
-    dq 0
